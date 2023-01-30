753692d565f023da78a6bcfed2db628eb257e0fc
Oyku <oykgokcek@gmail.com>
2023-01-30 15:51:46 +0300

    Completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,35 @@ export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
+export const SAVE_MEMO = "SAVE_MEMO";
+export const SEND_MEMO = "SEND_MEMO";
+export const CLEAR_MEMO = "CLEAR_MEMO";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+
 export const addOne = () => {
-    return({type:ADD_ONE});
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
+
+export const changeOperation = (operator) => {
+  return { type: CHANGE_OPERATION, payload: operator };
+};
+
+export const saveMemo = () => {
+  return { type: SAVE_MEMO };
+};
+
+export const sendMemo = () => {
+  return { type: SEND_MEMO };
+};
+
+export const clearMemo = () => {
+  return { type: CLEAR_MEMO };
+};
+
+export const clearDisplay = () => {
+  return { type: CLEAR_DISPLAY };
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,61 +1,121 @@
-import React from 'react';
+import React, { useReducer } from "react";
+import "./App.css";
+import reducer, { initialState } from "../reducers/index";
 
-import './App.css';
-
-import TotalDisplay from './TotalDisplay';
-import CalcButton from './CalcButton';
+import TotalDisplay from "./TotalDisplay";
+import CalcButton from "./CalcButton";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"> Reducer Challenge</a>
+        <a className="navbar-brand" href="#">
+          {" "}
+          Reducer Challenge
+        </a>
       </nav>
 
-      <div className = "container row mt-5">
+      <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-            
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <span id="operation">
+                <b>Operation:</b> {state.operation}
+              </span>
+              <span id="memory">
+                <b>Memory:</b> {state.memory}
+              </span>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton
+                value={"M+"}
+                onClick={() => dispatch({ type: "SAVE_MEMO" })}
+              />
+              <CalcButton
+                value={"MR"}
+                onClick={() => dispatch({ type: "SEND_MEMO" })}
+              />
+              <CalcButton
+                value={"MC"}
+                onClick={() => dispatch({ type: "CLEAR_MEMO" })}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton
+                value={1}
+                onClick={() => dispatch({ type: "APPLY_NUMBER", payload: 1 })}
+              />
+              <CalcButton
+                value={2}
+                onClick={() => dispatch({ type: "APPLY_NUMBER", payload: 2 })}
+              />
+              <CalcButton
+                value={3}
+                onClick={() => dispatch({ type: "APPLY_NUMBER", payload: 3 })}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton
+                value={4}
+                onClick={() => dispatch({ type: "APPLY_NUMBER", payload: 4 })}
+              />
+              <CalcButton
+                value={5}
+                onClick={() => dispatch({ type: "APPLY_NUMBER", payload: 5 })}
+              />
+              <CalcButton
+                value={6}
+                onClick={() => dispatch({ type: "APPLY_NUMBER", payload: 6 })}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton
+                value={7}
+                onClick={() => dispatch({ type: "APPLY_NUMBER", payload: 7 })}
+              />
+              <CalcButton
+                value={8}
+                onClick={() => dispatch({ type: "APPLY_NUMBER", payload: 8 })}
+              />
+              <CalcButton
+                value={9}
+                onClick={() => dispatch({ type: "APPLY_NUMBER", payload: 9 })}
+              />
+            </div>
+
+            <div className="row">
+              <CalcButton
+                value={"+"}
+                onClick={() =>
+                  dispatch({ type: "CHANGE_OPERATION", payload: "+" })
+                }
+              />
+              <CalcButton
+                value={"*"}
+                onClick={() =>
+                  dispatch({ type: "CHANGE_OPERATION", payload: "*" })
+                }
+              />
+              <CalcButton
+                value={"-"}
+                onClick={() =>
+                  dispatch({ type: "CHANGE_OPERATION", payload: "-" })
+                }
+              />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton
+                value={"CE"}
+                onClick={() => dispatch({ type: "CLEAR_DISPLAY" })}
+              />
             </div>
-
           </form>
         </div>
       </div>
